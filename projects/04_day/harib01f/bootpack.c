6905236f0b6091054bb6ca2fcc0c55b4df0bb12c
4927618350 <1426792204@qq.com>
2022-11-17 21:35:16 +0800

    OS

---
+++ projects/04_day/harib01f/bootpack.c
@@ -0,0 +1,71 @@
+void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+/* ���͓����\�[�X�t�@�C���ɏ����Ă����Ă��A��`����O�Ɏg���̂Ȃ�A
+	����ς�錾���Ă����Ȃ��Ƃ����Ȃ��B */
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+
+void HariMain(void)
+{
+	int i; /* �ϐ��錾�Bi�Ƃ����ϐ��́A32�r�b�g�̐����^ */
+	char *p; /* p�Ƃ����ϐ��́ABYTE [...]�p�̔Ԓn */
+
+	init_palette(); /* �p���b�g��ݒ� */
+
+	p = (char *) 0xa0000; /* �Ԓn���� */
+
+	for (i = 0; i <= 0xffff; i++) {
+		p[i] = i & 0x0f;
+	}
+
+	for (;;) {
+		io_hlt();
+	}
+}
+
+void init_palette(void)
+{
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00,	/*  0:�� */
+		0xff, 0x00, 0x00,	/*  1:���邢�� */
+		0x00, 0xff, 0x00,	/*  2:���邢�� */
+		0xff, 0xff, 0x00,	/*  3:���邢���F */
+		0x00, 0x00, 0xff,	/*  4:���邢�� */
+		0xff, 0x00, 0xff,	/*  5:���邢�� */
+		0x00, 0xff, 0xff,	/*  6:���邢���F */
+		0xff, 0xff, 0xff,	/*  7:�� */
+		0xc6, 0xc6, 0xc6,	/*  8:���邢�D�F */
+		0x84, 0x00, 0x00,	/*  9:�Â��� */
+		0x00, 0x84, 0x00,	/* 10:�Â��� */
+		0x84, 0x84, 0x00,	/* 11:�Â����F */
+		0x00, 0x00, 0x84,	/* 12:�Â��� */
+		0x84, 0x00, 0x84,	/* 13:�Â��� */
+		0x00, 0x84, 0x84,	/* 14:�Â����F */
+		0x84, 0x84, 0x84	/* 15:�Â��D�F */
+	};
+	set_palette(0, 15, table_rgb);
+	return;
+
+	/* static char ���߂́A�f�[�^�ɂ����g���Ȃ�����DB���ߑ��� */
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+	int i, eflags;
+	eflags = io_load_eflags();	/* ���荞�݋��t���O�̒l���L�^���� */
+	io_cli(); 					/* ���t���O��0�ɂ��Ċ��荞�݋֎~�ɂ��� */
+	io_out8(0x03c8, start);
+	for (i = start; i <= end; i++) {
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags);	/* ���荞�݋��t���O�����ɖ߂� */
+	return;
+}
